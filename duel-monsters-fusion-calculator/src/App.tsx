af9b5946394705e71f9ad562981dbac0f4a49d2c
Davin <davinjjimenez@gmail.com>
2026-06-16 21:44:14 -0700

    Remove unused code from App.tsx.

--- duel-monsters-fusion-calculator/src/App.tsx
+++ duel-monsters-fusion-calculator/src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import SearchPage from './components/search-page/SearchPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <SearchPage></SearchPage>
-  )
+  );
 }
 
 export default App
