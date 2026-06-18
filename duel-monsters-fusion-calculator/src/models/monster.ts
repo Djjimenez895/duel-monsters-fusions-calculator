31d781d636100ed9cd8160caad7a84dce8145aa2
Davin <davinjjimenez@gmail.com>
2026-06-17 18:01:37 -0700

    Add initial Monster interface.

---
+++ duel-monsters-fusion-calculator/src/models/monster.ts
@@ -0,0 +1,9 @@
+export interface Monster { 
+    name: string;
+    attribute: string;
+    monsterLevel: string;
+    type: string;
+    description: string;
+    attackPoints: number;
+    defensePoints: number;   
+}
