514a197e217804733b049f90628f70baf9b475de
Davin <davinjjimenez@gmail.com>
2026-06-20 11:24:05 -0700

    Add fusionRecipeDTO. This will be used by the API that returns possible fusions given a monster name.

---
+++ backend/src/dtos/fusionRecipeDTO.ts
@@ -0,0 +1,17 @@
+import type { MonsterAttribute, MonsterType } from "../generated/prisma/client";
+
+export interface MonsterDTO {
+    name: string;
+    attribute: MonsterAttribute;
+    monsterLevel: number | null;
+    type: MonsterType[];
+    description: string | null;
+    attackPoints: number | null;
+    defensePoints: number | null;
+    imageUrl: string | null;
+}
+
+export interface FusionRecipeDTO {
+    materials: MonsterDTO[];
+    fusionResult: MonsterDTO;
+}
