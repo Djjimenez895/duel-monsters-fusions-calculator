479b8358a29431c47a25c329a697ce8181b02c3e
Davin <davinjjimenez@gmail.com>
2026-06-20 14:42:44 -0700

    Add cors logic to app.ts.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import monsterRouter from "./routes/monsterRoute";
 import fusionRecipeRouter from "./routes/fusionRecipeRoute";
 
 export const app = express();
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+}));
 app.use(express.json());
 
 app.get("/", (_req, res) => {
