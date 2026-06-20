d57510473c34bbaae8dbb0c8be9e1c4f40e7a7d0
Davin <davinjjimenez@gmail.com>
2026-06-20 14:41:05 -0700

    Add frontend DTOs for monsters and fusion recipes.

---
+++ frontend/src/models/fusion-recipe-dto.ts
@@ -0,0 +1,18 @@
+import type { MonsterAttribute } from "./monster-attributes";
+import type { MonsterType } from "./monster-type";
+
+export interface MonsterDTO {
+    name: string;
+    attribute: MonsterAttribute;
+    monsterLevel: number | null;
+    type: MonsterType[];
+    description: string | null;
+    attackPoints: number | null;
+    defensePoints: number | null;
+    imageUrl: string | null;
+}
+
+export interface FusionRecipeDTO {
+    materials: MonsterDTO[];
+    fusionResult: MonsterDTO;
+}
