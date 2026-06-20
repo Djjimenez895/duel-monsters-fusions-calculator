d4aa89ab033697acc36036314dcf719f7eae0c26
Davin <davinjjimenez@gmail.com>
2026-06-19 21:06:38 -0700

    Update backend Monster model to support id, nullable fields, createdAt, and updatedAt.

--- backend/src/models/monster.ts
+++ backend/src/models/monster.ts
@@ -2,15 +2,18 @@ import type { MonsterAttribute } from "./monster-attributes";
 import type { MonsterType } from "./monster-type";
 
 /** Repesents a monster card in Duel Monsters.
- * 
+ *
  */
-export interface Monster { 
+export interface Monster {
+    id: number;
     name: string;
     attribute: MonsterAttribute;
     monsterLevel: number;
     type: MonsterType[];
-    description: string;
-    attackPoints: number;
-    defensePoints: number;
-    imageUrl?: string;
+    description: string | null;
+    attackPoints: number | null;
+    defensePoints: number | null;
+    imageUrl: string | null;
+    createdAt: Date;
+    updatedAt: Date;
 }
