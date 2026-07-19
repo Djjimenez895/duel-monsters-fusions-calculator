c7fc351a983fb72e9b8b70426b1c406770eb633f
Davin <davinjjimenez@gmail.com>
2026-07-19 10:52:52 -0700

    #3 Use duelist name mapping util in fusionRecipeService so that the names are returned properly formatted by the API.

--- backend/src/services/fusionRecipeService.ts
+++ backend/src/services/fusionRecipeService.ts
@@ -1,5 +1,22 @@
 import { findFusionRecipesByMaterialNamePrefix } from "../repositories/fusionRecipeRepository";
+import { duelistLabels } from "../utils/duelistLabels";
 import logger from "../logger";
+
+type MonsterWithDrops = Awaited<ReturnType<typeof findFusionRecipesByMaterialNamePrefix>>[number]["resultMonster"];
+
+function mapMonster(monster: MonsterWithDrops) {
+    return {
+        ...monster,
+        monsterCardDrops: monster.monsterCardDrops.map((drop) => ({
+            ...drop,
+            duelistName: duelistLabels[drop.duelistName],
+        })),
+        monsterVictoryBonuses: monster.monsterVictoryBonuses.map((bonus) => ({
+            ...bonus,
+            duelistName: duelistLabels[bonus.duelistName],
+        })),
+    };
+}
 
 export async function getFusionRecipesByMaterialNamePrefix(materialNamePrefix: string) {
     logger.info(`Searching for fusions for the monster: ${materialNamePrefix}`);
@@ -7,7 +24,7 @@ export async function getFusionRecipesByMaterialNamePrefix(materialNamePrefix: s
     logger.info(`Found ${recipes.length} recipe(s) for ${materialNamePrefix}`);
 
     return recipes.map((recipe) => ({
-        fusionResult: recipe.resultMonster,
-        materials: recipe.materials.map((m) => m.materialMonster),
+        fusionResult: mapMonster(recipe.resultMonster),
+        materials: recipe.materials.map((m) => mapMonster(m.materialMonster)),
     }));
 }
